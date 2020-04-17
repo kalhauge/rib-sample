add34d528ed8dd4760c14ba7195be85495cc1ebc
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-04-17 17:24:52 +0200

    Trying out a new Route system.
    The goal here is make the routes type-safe.  This means that if a route
    exist in the runtime code, that route will be populated on the server,
    or the server will throw an error.  Routes are therefore safe to
    reference in the html code.  This is usefull because the most common
    problem with static sites are broken links. With this setup, it will
    newer happen.
    
    Furthermore, it has some other benifits:
    
    1) The server can do custom url actions. For example add
      a base url or it could hash the static files and add them
      as a suffix.
    
    2) It could be highly parallisable.
    
    3) It has total control of all routes on the servers, this means
       it will be able to generate a sitemap trivially.
       (https://github.com/srid/zulip-archive/issues/10)

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Main where
 
@@ -13,32 +19,137 @@ import Control.Monad
 import Data.Aeson (FromJSON, fromJSON)
 import qualified Data.Aeson as Aeson
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import Data.Text (Text)
 import Development.Shake
 import GHC.Generics (Generic)
 import Lucid
 import Main.Utf8
-import Rib (IsRoute, Pandoc)
+-- import Rib (IsRoute, Pandoc)
+import Rib (Pandoc)
 import qualified Rib
 import qualified Rib.Parser.Pandoc as Pandoc
 import System.FilePath
 
--- | Route corresponding to each generated static page.
---
--- The `a` parameter specifies the data (typically Markdown document) used to
--- generate the final page text.
-data Route a where
-  Route_Index :: Route [(Route Pandoc, Pandoc)]
-  Route_Article :: FilePath -> Route Pandoc
+-- base
+import Data.Typeable
 
--- | The `IsRoute` instance allows us to determine the target .html path for
--- each route. This affects what `routeUrl` will return.
-instance IsRoute Route where
-  routeFile = \case
-    Route_Index ->
-      pure "index.html"
-    Route_Article srcPath ->
-      pure $ "article" </> srcPath -<.> ".html"
+-- transformers
+import Control.Monad.Writer
+
+-- path
+import Path
+
+-- Some definitions
+
+-- | A route is a local path to an destination.
+-- The implemenation is explictly hidden from the user to avoid the 
+-- user create routes that do not exist.
+data Route = 
+  Route (Path Abs Dir) (Path Rel File)
+
+-- | We can get the url of a route.
+routeUrl :: Route -> Text
+routeUrl (Route burl url) =  
+  T.pack . toFilePath $ burl Path.</> url
+
+-- | We can use the url directly in a a lucid attribute. Use instead of 
+-- 'Lucid.href_'
+rref_ :: Route -> Attribute
+rref_ = href_  . routeUrl
+
+-- | A top-level monad to control the generation of a Site.
+-- It is essentially a Write monad which stores all the routes needed 
+-- to generate the site. 
+newtype SiteGen s a = 
+  SiteGen (WriterT [(Route, (s -> Action TL.Text))] Action a)
+  deriving newtype (Functor, Applicative, Monad, MonadFail)  
+
+liftAction :: Action a -> SiteGen s a
+liftAction m = SiteGen (lift m)
+
+newtype SiteConfig = SiteConfig 
+  { baseUrl :: Path Abs Dir
+  } deriving (Typeable)
+
+-- | Create a route
+makeRoute :: 
+  Path Rel File 
+  -- | The relative name of the route
+  -> (s -> Action TL.Text) 
+  -- | The action required to make the route
+  -> SiteGen s Route
+makeRoute rel act = do
+  -- At SomePoint
+  -- Just (SiteConfig { baseUrl }) <- liftAction getShakeExtra
+  let route = Route [absdir|/|] rel
+  SiteGen (tell [(route, act)])
+  return $ route
+ 
+-- | Create an HTML Route
+writeHtmlRoute :: 
+  Path Rel File 
+  -> (s -> Action (Html ()))
+  -> SiteGen s Route
+writeHtmlRoute r f = 
+  makeRoute r ( fmap (Lucid.renderText) . f )
+
+-- | Like the Rib version but over SiteGen.
+forEvery :: 
+  [Path Rel File] 
+  -> (Path Rel File -> SiteGen s a) 
+  -> SiteGen s [a]
+forEvery paths f = do
+  fs <- liftAction $ do 
+    input <- Rib.ribInputDir
+    fs <- getDirectoryFiles' input paths
+    return fs
+  forM fs f
+
+ where
+  -- | Like `getDirectoryFiles` but works with `Path`
+  getDirectoryFiles' :: FilePath -> [Path Rel File] -> Action [Path Rel File]
+  getDirectoryFiles' dir (fmap toFilePath -> pat) =
+    traverse (liftIO . parseRelFile) 
+      =<< getDirectoryFiles dir pat
+
+
+-- | Get a route to a Static file.
+buildStaticFile :: Path Rel File -> SiteGen s Route
+buildStaticFile p = do
+  makeRoute p . const $ do 
+    input <- Rib.ribInputDir
+    TL.pack <$> readFile' (input System.FilePath.</> toFilePath p)
+
+newRun :: 
+  FilePath 
+  -> FilePath 
+  -> SiteGen s s 
+  -> IO ()
+newRun content dest (SiteGen gen) = do
+  Rib.run content dest $ do 
+    (site, routes) <- runWriterT gen
+    void $ forP routes $ \(Route _ path, generator) -> do
+      txt <- generator site
+      Rib.writeFileCached (toFilePath path) (TL.unpack txt)
+
+
+-- FROM HERE ON IS THE REAL SAMPLE CODE
+
+-- | Specify Post Data
+data Post = Post
+  { postTitle       :: Text
+  , postDescription :: Maybe Text
+  , postRoute       :: Route
+  }
+
+-- | Specify the Site Routes
+data Site = Site
+  { siteStaticReadme :: Route
+  , siteIndex :: Route
+  , sitePosts :: [ Post ]
+  }
+
 
 -- | Main entry point to our generator.
 --
@@ -52,53 +163,72 @@ instance IsRoute Route where
 -- provided by Rib to do the actual generation of your static site.
 main :: IO ()
 main = withUtf8 $ do
-  Rib.run "content" "dest" generateSite
+  newRun "content" "dest" generateSite
 
 -- | Shake action for generating the static site
-generateSite :: Action ()
+generateSite :: SiteGen Site Site
 generateSite = do
   -- Copy over the static files
-  Rib.buildStaticFiles ["static/**"]
-  let writeHtmlRoute :: Route a -> a -> Action ()
-      writeHtmlRoute r = Rib.writeRoute r . Lucid.renderText . renderPage r
+  siteStaticReadme <- 
+    buildStaticFile [relfile|static/README.md|]
+  
+  -- Render the Site Index
+  siteIndex <- writePage [relfile|index.html|] $ \Site { sitePosts } -> Page 
+    { pageTitle = "My Home Page"
+    , pageBody = do
+        div_ $ forM_ sitePosts $ \Post { .. } ->
+          li_ [class_ "pages"] $ do
+            b_ $ a_ [rref_ postRoute] $ toHtml $ postTitle
+            renderMarkdown `mapM_` postDescription
+    }
+
   -- Build individual sources, generating .html for each.
-  articles <-
-    Rib.forEvery ["*.md"] $ \srcPath -> do
-      let r = Route_Article srcPath
-      doc <- Pandoc.parse Pandoc.readMarkdown srcPath
-      writeHtmlRoute r doc
-      pure (r, doc)
-  writeHtmlRoute Route_Index articles
+  sitePosts <-
+    Main.forEvery [[relfile|*.md|]] $ \srcPath -> do
+      doc <- liftAction $ 
+        Pandoc.parse Pandoc.readMarkdown (toFilePath srcPath)
+
+      let SrcMeta { title, description } = getMeta doc
+      postRoute <- writePage srcPath $ \_ -> Page 
+        { pageTitle = toHtml title
+        , pageBody = 
+            article_ (Pandoc.render doc)
+        }
+      
+      pure $ Post 
+        { postRoute 
+        , postTitle = title
+        , postDescription = description
+        }
+
+  return $ Site {..}
+
+ where 
+  renderMarkdown :: Text -> Html ()
+  renderMarkdown =
+    Pandoc.render . Pandoc.parsePure Pandoc.readMarkdown
+  
+  writePage :: Path Rel File -> (Site -> Page) -> SiteGen Site Route
+  writePage rel fn = 
+    writeHtmlRoute rel (\s -> pure $ renderPage (fn s) s)
+
+data Page = Page
+  { pageTitle :: Html ()
+  , pageBody  :: Html ()
+  }
 
 -- | Define your site HTML here
-renderPage :: Route a -> a -> Html ()
-renderPage route val = html_ [lang_ "en"] $ do
+renderPage :: Page -> Site -> Html ()
+renderPage Page { .. } Site { siteIndex } = html_ [lang_ "en"] $ do
   head_ $ do
     meta_ [httpEquiv_ "Content-Type", content_ "text/html; charset=utf-8"]
-    title_ routeTitle
+    title_ pageTitle
     style_ [type_ "text/css"] $ C.render pageStyle
   body_ $ do
     div_ [class_ "header"] $
-      a_ [href_ "/"] "Back to Home"
-    h1_ routeTitle
-    case route of
-      Route_Index ->
-        div_ $ forM_ val $ \(r, src) ->
-          li_ [class_ "pages"] $ do
-            let meta = getMeta src
-            b_ $ a_ [href_ (Rib.routeUrl r)] $ toHtml $ title meta
-            renderMarkdown `mapM_` description meta
-      Route_Article _ ->
-        article_ $
-          Pandoc.render val
-  where
-    routeTitle :: Html ()
-    routeTitle = case route of
-      Route_Index -> "Rib sample site"
-      Route_Article _ -> toHtml $ title $ getMeta val
-    renderMarkdown :: Text -> Html ()
-    renderMarkdown =
-      Pandoc.render . Pandoc.parsePure Pandoc.readMarkdown
+      a_ [rref_ siteIndex ] "Back to Home"
+    h1_ pageTitle
+    pageBody
 
 -- | Define your site CSS here
 pageStyle :: Css
